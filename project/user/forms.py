e859a093770bc883e95905d3159961d8d61fa82e
Amit Parkar <83871869+amitp0@users.noreply.github.com>
2022-03-27 21:49:34 +0530

    Recent changes after review

--- project/user/forms.py
+++ project/user/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from crispy_forms.layout import Layout,Field
+from crispy_forms.helper import FormHelper
+from numpy import true_divide
+from user.models import Employee
 
 
 class UserRegisterForm(UserCreationForm):
@@ -23,3 +27,23 @@ class UserRegisterForm(UserCreationForm):
 		if commit:
 			user.save()
 		return user
+
+class EmployeeForm(forms.ModelForm):
+	emp_id=forms.IntegerField(required=True)
+	emp_name=forms.CharField(required=True)
+	company_id=forms.IntegerField(required=True)
+	domain=forms.CharField(required=True)
+	ready_to_relocate=forms.BooleanField(required=True)
+	current_loc=forms.CharField(required=True)
+	resume_link=forms.URLField(required=False)
+	notice_period=forms.DateField(required=True)
+	laidoffdate=forms.DateField(required=True)
+
+	class Meta:
+		model=Employee
+		fields=['emp_id','emp_name','company_id','domain','ready_to_relocate','current_loc','resume_link','notice_period','laidoffdate']
+		
+	# def __init__(self, *args, **kwargs):
+	# 	super().__init__(*args, **kwargs)
+	# 	self.helper = FormHelper()
+	# 	self.helper.layout = Layout(Field('domain', type='hidden'))
--- project/user/views.py
+++ project/user/views.py
@@ -1,3 +1,4 @@
+from tkinter import N
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import UserRegisterForm
+from .forms import EmployeeForm, UserRegisterForm
 from .models import Company, Employee
 from django.contrib.auth.decorators import login_required
 
@@ -22,13 +23,23 @@ from sklearn.metrics.pairwise import sigmoid_kernel
 def top_recommend(request):
     d2 = pd.read_csv(r"C:\Users\admin\Desktop\dj-sme\project\user\media\database.csv")
     d2 = d2.nlargest(6, ["ratings"])
-    return d2["name"]
+    d2 = pd.DataFrame(d2)
+    n1=d2['name']
+    n2=d2['industry_new']
+    n3=d2['rating']
+    tp=tuple(zip(n1,n2,n3))
+    return tp
 
 
 def new_adds(request):
     d2 = pd.read_csv(r"C:\Users\admin\Desktop\dj-sme\project\user\media\database.csv")
     d2 = d2.tail(6)
-    return d2["name"]
+    d2 = pd.DataFrame(d2)
+    n1=d2['name']
+    n2=d2['industry_new']
+    n3=d2['rating']
+    tna=tuple(zip(n1,n2,n3))
+    return tna
 
 
 def give_recommendation(name):
@@ -45,6 +56,8 @@ def give_recommendation(name):
     tfv_matrix = tfv.fit_transform(d2["r_lemmatized"])
     sig = sigmoid_kernel(tfv_matrix, tfv_matrix)
     l = []
+    ir= []
+    r= []
     index = d2[d2["name"] == name].index[0]
     sig_scores = list(enumerate(sig[index]))
     sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
@@ -52,12 +65,26 @@ def give_recommendation(name):
     comp_indices = [i[0] for i in sig_scores]
     for i in comp_indices:
         val = d2["name"].iloc[i]
+        ind= d2["industry_new"].iloc[i]
+        rating=d2["rating"].iloc[i]
         l.append(val)
-    return l
+        ir.append(str(ind))
+        r.append(rating)
+    tp=tuple(zip(l,ir,r))
+    return tp
 
-
-# on hitting url endpoint home/recommend will be redirected here
-
+def give_rec2(username):
+    r=Company.objects.get(username=username)
+    print(r.company_domain)
+    dataframe=pd.read_csv(r'C:\Users\admin\Desktop\dj-sme\project\user\media\database.csv')
+    rslt_df = dataframe[dataframe['industry_new'] == r.company_domain]
+    rslt_df=pd.DataFrame(rslt_df)
+    rslt_df=rslt_df.head(11)
+    name=rslt_df['name']
+    ind=rslt_df['industry_new']
+    rat=rslt_df['rating']
+    pred=tuple(zip(name,ind,rat))
+    return pred
 
 def index(request):
     return render(request, "user/index.html")
@@ -65,33 +92,29 @@ def index(request):
 
 @login_required
 def dashboard(request):
+    
     if request.user.is_authenticated:
         username = request.user.username
     else:
         print("::::::::::::::::::::")
-    print(";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;"+username)
+    print(";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;"+username+";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
 
     try:
         pred = give_recommendation(username)
+        print("INTERNAL RECC")
+        print("ooooooooooooooooooooooooooooooo"+"Not in dataset"+"ooooooooooooooooooooooooooooooo")
     except:
-        print("ooooooooooooooooooooooooooooooo"+"Not in dataset")
+        pred=give_rec2(username)
+        print("///////")
+
+    
 
         
-    try:
-        r=Company.objects.get(username=username)
-        print(r.company_domain)
-        dataframe=pd.read_csv(r'C:\Users\admin\Desktop\dj-sme\project\user\media\result.csv')
-        rslt_df = dataframe[dataframe['industry'] == r.company_domain]
-        rslt_df=pd.DataFrame(rslt_df)
-        rslt_df=rslt_df.head(11)
-        pred=rslt_df['name']
-        print(pred)
-    except:
-        print("/////////////////////")
 
 
     newad=new_adds(request)
     toprec=top_recommend(request)
+    print("************************************************************")
     context = {'pred': pred,'new':newad,'top':toprec}
     return render(request, 'user/dashboard.html', context)
 
@@ -153,3 +176,51 @@ def recommend(request):
     except:
         print("Company does not exist in database")
         return redirect("signup")
+
+def user_page(request,username):
+    r=Company.objects.get(username=username)
+    return render(request,)
+
+
+@login_required
+def addEmployee(request):
+    if request.method=="POST":
+        form=EmployeeForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/show')
+            except:
+                pass
+    else:
+            form= EmployeeForm()
+    return render(request,'user/s.html',{'form':form})
+
+@login_required
+def show(request):
+    sample_instance = Company.objects.get(username=request.user.username)
+    value_of_name = sample_instance.comp_id
+    print(value_of_name)
+    # print("fthdhdhhx")
+    employees = Employee.objects.filter(company_id=value_of_name)  
+    return render(request,"user/show.html",{'employees':employees}) 
+
+@login_required
+def edit(request, id):  
+    employee = Employee.objects.get(emp_id=id)  
+    return render(request,'user/edit.html', {'employee':employee})
+
+@login_required
+def update(request, id):  
+    employee = Employee.objects.get(emp_id=id)  
+    form = EmployeeForm(request.POST, instance = employee)
+    if form.is_valid():  
+        form.save()  
+        return redirect("/show")  
+    return render(request, 'user/edit.html', {'employee': employee})  
+
+@login_required
+def delete(request, id):  
+    employee = Employee.objects.get(emp_id=id)  
+    employee.delete()  
+    return redirect("/show")  
